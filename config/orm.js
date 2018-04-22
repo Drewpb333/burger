e48d83ede9374f860d31cb5cef665169b551773c
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-04-22 10:52:51 -0400

    added obj to sql conversion functions

--- config/orm.js
+++ config/orm.js
@@ -3,50 +3,63 @@ var connection = require("./connection.js");
 //allots for proper number of ? marks
 function printQMarks(num) {
     var arr = [];
-  
     for (var i = 0; i < num; i++) {
-      arr.push("?");
+        arr.push("?");
     }
-  
     return arr.toString();
-  }
+}
 
-//Object of Methods for Object Relational Mapping
-var orm = {
-    selectAll: function (table, callback) {
-        var queryString = "SELECT * FROM" + table + ";";
-        connection.query(queryString, function (err, result) {
-            if (err) throw err;
-            callback(result);
-        });
-    },
-    insertOne: function (table, col, vals, callback) {
-        var queryString = "INSERT INTO" + table;
+//converts key values pairs to proper SQL syntax
+function convertToSql(obj) {
+    var arr = [];
 
-        //prevents SQL injection
-        queryString += " (";
-        queryString += col.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQMarks(vals.length);
-        queryString += ");";
-    
-        connection.query(queryString, [burgerName], function (result) {
-            if (err) throw err;
-            callback(result);
-        });
-    },
-    updateOne: function (burgerName) {
-        var queryString = "UPDATE burgers SET devoured TRUE WHERE burger_name= ?";
-        connection.query(queryString, [isDevoured, burgerName], function (err, result) {
-            if (err) throw err;
-            console.log(result);
-        });
+    // fills array with newly created SQL syntax
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            //maintains string property in SQL
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // converts colon to equal sign
+            arr.push(key + "=" + value);
+        }
     }
-};
+}
+ 
+    //Object of Methods for Object Relational Mapping
+    var orm = {
+        selectAll: function (table, callback) {
+            var queryString = "SELECT * FROM " + table + ";";
+            connection.query(queryString, function (err, result) {
+                if (err) throw err;
+                callback(result);
+            });
+        },
+        insert: function (table, col, vals, callback) {
+            var queryString = "INSERT INTO " + table;
 
-module.exports = orm;
+            //allows for proper formatting from object to sql syntax
+            queryString += " (" + col.toString() + ") ";
+            queryString += "VALUES (" + printQMarks(vals.length) + ");";
 
+            connection.query(queryString, vals, function (err, result) {
+                if (err) throw err;
+                callback(result);
+            });
+        },
+        update: function (table, ObjColNames, condition, callback) {
+            var queryString = "UPDATE " + table;
 
+            //allows for proper formatting from object to sql syntax
+            queryString += " SET " + convertToSql(ObjColNames);
+            queryString += " WHERE " + condition;
 
-module.exports = orm;
+            connection.query(queryString, function (err, result) {
+                if (err) throw err;
+                callback(result);
+            });
+        }
+    };
+
+    module.exports = orm;

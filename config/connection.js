290264da265e7e9cb60afa1fdf355fca5f3bc8eb
Drewpb333 <34869642+Drewpb333@users.noreply.github.com>
2018-04-24 19:48:43 -0400

    heroku connection added to connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,9 +8,22 @@ var connection = mysql.createConnection({
     database: "burgers_db"
 });
 
+//for deploying on Heroku
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "drewpb333",
+        database: "burgers_db"
+    })
+}
+
+
 //connects to mysql db
 connection.connect(function (err) {
-    if(err){
+    if (err) {
         return console.log(err);
     }
     //verifies that connection has been made
